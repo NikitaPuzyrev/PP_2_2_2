907151234698bd409511d8fb76ae003e1d11b42c
Nikita <nkt@rambler.ru>
2022-08-12 11:37:51 +0300

    q

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -7,20 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.Service;
+import web.service.ServiceCar;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    @GetMapping()
-    String getCar(Model model, @RequestParam(value = "count", required = false) String count) {
 
-        List<Car> list;
-        if (count != null){
-            list = Service.getCarlist(count);
-        } else{
-            list = Service.getCarlist("count");
+    Service service = new Service();
+    List<Car> list;
+
+    @GetMapping()
+    String getCar(Model model, @RequestParam(value = "count", required = false) Integer count) {
+
+        if (count != null) list = service.getCarlist(count);
+        else {
+            list = service.getCarlist(10);
         }
         model.addAttribute("car", list);
         return "car";
--- PP_2_2_2_spring-mvc-master/src/main/java/web/service/Service.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/service/Service.java
@@ -1,32 +1,26 @@
 package web.service;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-
 import java.util.Arrays;
 import java.util.List;
 
 @Component
-public class Service {
-    static List<Car> carlist = Arrays.asList(
+public class Service implements ServiceCar {
+    List<Car> carlist = Arrays.asList(
             new Car("Yo", "petrol", "red"),
             new Car("Yo1", "diesel", "black"),
             new Car("Yo2", "el", "green"),
             new Car("Yo3", "petrol", "orange"),
             new Car("Yo4", "diesel", "brown"));
 
-
-    public static List<Car> getCarlist(String ncar) {
-        switch (ncar) {
-            case "2":
-                return carlist.subList(0, 2);
-            case "3":
-                return carlist.subList(0, 3);
-            case "4":
-                return carlist.subList(0, 4);
-            default:
-                return carlist;
+    @Override
+    public List<Car> getCarlist(int ncar) {
+        if (ncar < 5 && ncar > 1) {
+            return carlist.subList(1, ncar + 1);
+        } else {
+            return carlist;
         }
     }
 }
+
